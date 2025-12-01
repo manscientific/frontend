9168d1e95069e904ccb0d33cc92b55018883aec3
SHIVEN KUMAR <manscientificks@gmail.com>
2025-12-01 22:19:55 +0530

    last try

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://weather-crop-advisory.onrender.com",
+  baseURL: "http://localhost:8000",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
